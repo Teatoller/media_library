30647aefb2e7633dd38e8b9e143148540f3ca0dc
Teatoller <steven.otieno1@gmail.com>
2019-09-07 22:01:27 +0300

    (form): Implement feedback form
    - create form to accept suggestions
    - create temporary server page
    - add a thank you message in separate page
    
    [Starts #009]

--- suggest.php
+++ suggest.php
@@ -1,11 +1,32 @@
-<?php 
+<?php
 $pageTitle = "Suggest a Media Item";
 $section = "suggest";
 
 include("inc/header.php"); ?>
 
 <div class="section page">
-    <h1>Suggest a Media Item</h1>
+    <div class="wrapper">
+        <h1>Suggest a Media Item</h1>
+        <p>Complete the form and send me an email, if you think there is something I am missing</p>
+        <form method="post" action="process.php">
+            <table>
+                <tr>
+                    <th><label for="name">Name</label></th>
+                    <td><input type="text" name="name" id="name"></td>
+                </tr>
+                <tr>
+                    <th><label for="email">Email</label></th>
+                    <td><input type="text" name="email" id="email"></td>
+                </tr>
+                <tr>
+                    <th><label for="details">Suggest Item Details</label></th>
+                    <td><textarea name="details" id="details"></textarea></td>
+                </tr>
+            </table>
+            <input type="submit" value="send">
+        </form>
+    </div>
+
 </div>
 
 <?php include("inc/footer.php"); ?>

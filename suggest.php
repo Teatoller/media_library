54a849f80074f7a78b7192142c703e5536cc2a06
Teatoller <steven.otieno1@gmail.com>
2019-09-08 07:37:34 +0300

    (validators): Implement code refractor and validators
    - refactor code to have server output, thank you message and input fields in a single page
    - add validator to check for empty field or null value
    
    [Starts #010]

--- suggest.php
+++ suggest.php
@@ -1,4 +1,26 @@
 <?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = trim($_POST["name"]);
+    $email = trim($_POST["email"]);
+    $details = trim($_POST["details"]);
+
+    if ($name == '' || $email == '' || $details == ''){
+        echo "Please fill in the required fields: Name, Email and Details";
+        exit;
+    }
+
+    echo "<pre>";
+    $email_body = '';
+    $email_body .= "Name " . $posts['name'] . "\n";
+    $email_body .= "Email " . $posts['email'] . "\n";
+    $email_body .= "Details " . $posts['details'] . "\n";
+    echo $email_body;
+    echo "</pre>";
+    // To do: send email
+    header("location:suggest.php?status=thanks");
+}
+
 $pageTitle = "Suggest a Media Item";
 $section = "suggest";
 
@@ -7,24 +29,28 @@ include("inc/header.php"); ?>
 <div class="section page">
     <div class="wrapper">
         <h1>Suggest a Media Item</h1>
-        <p>Complete the form and send me an email, if you think there is something I am missing</p>
-        <form method="post" action="process.php">
-            <table>
-                <tr>
-                    <th><label for="name">Name</label></th>
-                    <td><input type="text" name="name" id="name"></td>
-                </tr>
-                <tr>
-                    <th><label for="email">Email</label></th>
-                    <td><input type="text" name="email" id="email"></td>
-                </tr>
-                <tr>
-                    <th><label for="details">Suggest Item Details</label></th>
-                    <td><textarea name="details" id="details"></textarea></td>
-                </tr>
-            </table>
-            <input type="submit" value="send">
-        </form>
+        <?php if (isset($_GET["status"]) && $_GET["status"] == "thanks") {
+            echo "<p>Thanks for the email! I&rsquo;ll check out your suggestions shortly!</p>";
+        } else { ?>
+            <p>Complete the form and send me an email, if you think there is something I am missing</p>
+            <form method="post" action="suggest.php">
+                <table>
+                    <tr>
+                        <th><label for="name">Name</label></th>
+                        <td><input type="text" name="name" id="name"></td>
+                    </tr>
+                    <tr>
+                        <th><label for="email">Email</label></th>
+                        <td><input type="text" name="email" id="email"></td>
+                    </tr>
+                    <tr>
+                        <th><label for="details">Suggest Item Details</label></th>
+                        <td><textarea name="details" id="details"></textarea></td>
+                    </tr>
+                </table>
+                <input type="submit" value="send">
+            </form>
+        <?php } ?>
     </div>
 
 </div>
